5ad80f3f829c16bb4bd31f10e784c0c4baecf7a2
Flavia Costa <FlaviaCo@nipromed.local>
2021-11-23 10:35:58 -0300

    Adicionado alinhamento dos botões Centralizado wrapper dos botões cancel e next

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -6,3 +6,19 @@ navToggle.addEventListener('click', () => document.body.classList.toggle('nav-op
 navLinks.forEach(link => {
     link.addEventListener('click', () => document.body.classList.remove('nav-open'));
 })
+function fecharModal()
+{
+  document.getElementById('fundo').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
+}
+
+function abrirModal()
+{
+  document.getElementById('fundo').style.display = 'block';
+  document.getElementById('modal').style.display = 'block';
+}
+var inputs = $('input').on('keyup', verificarInputs);
+function verificarInputs() {
+    const preenchidos = inputs.get().every(({value}) => value)
+    $('button-button-direita').prop('disabled', !preenchidos);
+}
